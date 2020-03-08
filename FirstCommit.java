2ccb4c5a39b40bcfa4f4491307825b358ae9700e
grn638 <60710581+grn638@users.noreply.github.com>
2020-03-08 17:18:40 -0500

    Add files via upload

---
+++ FirstCommit.java
@@ -0,0 +1,24 @@
+/**
+ * CS 3443 - Lab 5
+ *
+ * First Commit
+ *    Add a print statement to the bottom of the main method below.
+ *    Your print must contain your full name.
+ *
+ *    Follow the lab instructions carefully!
+ *    If you get a merge conflict, refer to course notes & slides for help!
+ */
+public class FirstCommit {
+
+	public static void main( String[] args ) {
+		System.out.println( "My name is Heena Rathore and my hobby is travelling" );
+		System.out.println( "My name is Bruce Orcutt and my hobby is composing modern classical music" );
+		System.out.println( "My name is Hunter Johnson and my hobby is looking at computer parts and wishing I had more money.");
+		System.out.println( "My name is Yi He and my hobby is watching animes." );
+		System.out.println( "My name is Paul Woody and my hobby is triathlons" );
+		System.out.println( "My name is Jennifer Ingram and my hobby is watching movies.")
+		System.out.println( "My name is Desiree Garcia and I like rock climbing." );
+		System.out.println( "My name is Jarrod Ragsdale and my hobby is mountain biking.");
+		System.out.println( "My name is Jennifer Bailey and my hobby is writing.");
+	}
+}
